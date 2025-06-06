ac05d57cd465626fe754db2642caad54b963077d
huhu <sunmy.huhu@gmail.com>
2025-06-06 13:42:22 +0800

    This is a test.

---
+++ pages/test-custom.js
@@ -0,0 +1,9 @@
+export default function MyCustomPage() {  
+    return (  
+      <div>  
+        <h1>我的自定义页面</h1>  
+        <p>这里可以写任何 HTML 内容</p>  
+        {/* 您的自定义 HTML/React 代码 */}  
+      </div>  
+    )  
+  }
